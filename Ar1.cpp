78aa07509c0e58558d1f5fcecf2df0a41d257b38
Marcin Szymczak <92090671+maruchiso@users.noreply.github.com>
2023-11-19 22:45:32 +0100

    Add files via upload
    Algorithm is working, TODO:
    -working on other canal not only on grayscale
    - working on ROI not only on whole img
    - adaptation version?

--- Ar1.cpp
+++ Ar1.cpp
@@ -3,54 +3,111 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <unordered_map>
 #include <vector>
 #include <math.h>
 #include <map>
+
 //20	314910	Mo2	Ar1
 
+//TODO: -Algorytm gotowy, -poszczególne kana³y, -wybór ROI, -wersja adaptacyjna?
+class Project1 {
 
-void ar1(cv::Mat img) {
-	//mapujemy ze sob¹ intensywnoœæ (0 - 255) i czêstoœæ z jak¹ wystêpuj¹ piksele o danej wartoœci intensywnoœci
-	std::unordered_map<int, int> histogram;
-	for (int i = 0; i < img.rows; i++) {
-		for (int j = 0; j < img.cols; j++) {
-			int intensity = static_cast<int>(img.at<uchar>(i, j));
-			histogram[intensity]++;
+	cv::Mat img;
+	std::map<int, int> histogram;
+	std::map<int, int> equlized_histogram;
+	std::vector<double> cdf;
+
+public:
+	Project1(const std::string& path) {
+		img = cv::imread(path, cv::IMREAD_GRAYSCALE);
+		if (img.empty()) {
+			std::cerr << "B³¹d: Nie uda³o siê wczytaæ obrazu." << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		else {
+			print_original_image();
+
+			calculate_histogram();
+			print_histogram();
+
+			calculate_cdf();
+			histogram_equalization();
+
+			calculate_histogram();
+			print_histogram();
+
+			print_equalized_image();
 		}
 	}
-	for (const auto& row : histogram) {
-		std::cout << row.first << ", " << row.second << std::endl;
+
+	void calculate_histogram() {
+
+		for (unsigned int i = 0; i < img.rows; i++) {
+			for (unsigned int j = 0; j < img.cols; j++) {
+				int intensity = static_cast<int>(img.at<uchar>(i, j));
+				histogram[intensity]++;
+			}
+		}
+
+		for (int i = 0; i <= 255; ++i) {
+			if (histogram.count(i) <= 0) {
+				histogram[i] = 0;
+			}
+			
+		}
 	}
-	std::cout << "------------------" << std::endl;
-	//probability of occurance of a pixel
-	std::unordered_map<int, int> cdf;
-	int sum = 0;
-	//print probability
-	for (auto& row : histogram) {
-		row.second = round(row.second / 256);
-		sum += row.second;
+
+	void calculate_cdf() {
+
+		double sum = 0.0;
 		
-		std::cout << row.first << ", " << row.second << std::endl;
+		for (auto& row : histogram) {
+			sum += row.second;
+			cdf.push_back(sum);
+		}
+
+		//Normalize CDF
+		for (double& x : cdf) {
+			x /= sum;
+		}
 		
 	}
-	std::unordered_map<int, int> equalized_histogram;
 
+	void histogram_equalization() {
+		for (unsigned int i = 0; i < img.rows; i++) {
+			for (unsigned int j = 0; j < img.cols; j++) {
+				int intensity = static_cast<int>(img.at<uchar>(i, j));
+				img.at<uchar>(i, j) = static_cast<uchar>(cdf[intensity] * 255.0);
+			}
 
-}
-class 
-void equalization_histogram(cv::Mat img) {
+		}
+	}
 
-}
+	void print_original_image() {
+		cv::imshow("Original image", img);
+		cv::waitKey(0);
+	}
+
+	void print_histogram() {
+		std::cout << "Histogram:\n";
+		for (auto& row : histogram) {
+			std::cout << row.first << ", " << row.second << std::endl;
+		}
+		std::cout << std::endl << std::endl << std::endl;
+	}
+	void print_equalized_image() {
+		cv::imshow("Equalized image", img);
+		cv::waitKey(0);
+		std::cout << "Histogram dla drugiego zdjecia: \n";
+		
+	}
+};
 int main() {
-	
-	std::string path = "C:/Users/marcin/Desktop/na insta dêbki23/IMG_9903.png";
-	cv::Mat img = cv::imread(path, cv::IMREAD_GRAYSCALE);
-	//cv::resize(img, img, { 512, 512 }, 0, 0, cv::INTER_NEAREST);
-	ar1(img);
-	//cv::imshow("Image", img);
-	//cv::waitKey(0);
-	
-	
+
+	//std::string path = "C:/Users/marcin/Desktop/na insta dêbki23/IMG_9903.png";
+	std::string path = "C:/Users/marcin/Desktop/test.jpg";
+
+
+	Project1 project(path);
 	return 0;
 }

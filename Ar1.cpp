540e3c00ae47f70cc2937988e2b006c074625429
Marcin Szymczak <92090671+maruchiso@users.noreply.github.com>
2023-11-20 23:26:53 +0100

    Add files via upload
    Added dilatate function

--- Ar1.cpp
+++ Ar1.cpp
@@ -10,7 +10,7 @@
 //20	314910	Mo2	Ar1
 
 //TODO: -Algorytm gotowy, -poszczególne kana³y, -wybór ROI, -wersja adaptacyjna?
-class Project1 {
+class Project1_Ar1 {
 
 	cv::Mat img;
 	std::map<int, int> histogram;
@@ -18,7 +18,7 @@ class Project1 {
 	std::vector<double> cdf;
 
 public:
-	Project1(const std::string& path) {
+	Project1_Ar1(const std::string& path) {
 		img = cv::imread(path, cv::IMREAD_GRAYSCALE);
 		if (img.empty()) {
 			std::cerr << "B³¹d: Nie uda³o siê wczytaæ obrazu." << std::endl;
@@ -102,12 +102,54 @@ public:
 		
 	}
 };
+
+class Project1_Mo2 {
+private:
+	cv::Mat img;
+	cv::Mat output;
+public:
+	Project1_Mo2(std::string path, bool isBinary) {
+		img = cv::imread(path, cv::IMREAD_GRAYSCALE);
+		if (isBinary == false) binarization();
+	}
+	void binarization() {
+		cv::threshold(img, img, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+	}
+	void dilitation(int kernelSize) {
+		output = img.clone();
+		for (unsigned int i = 0; i < img.rows; i++) {
+			for (unsigned int j = 0; j < img.cols; j++) {
+				if (img.at<uchar>(i, j) == 255) {
+					for (int x = -kernelSize / 2; x <= kernelSize; x++) {
+						for (int y = -kernelSize / 2; y <= kernelSize; y++) {
+							int nx = i + x;
+							int ny = j + y;
+							if (nx >= 0 && nx < img.rows && ny >= 0 && ny < img.cols)
+								output.at<uchar>(nx, ny) = 255;
+
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	void print_img() {
+		cv::imshow("Equalized image", output);
+		cv::waitKey(0);
+	}
+
+	void erosion() {
+		//TODO
+	}
+};
 int main() {
 
 	//std::string path = "C:/Users/marcin/Desktop/na insta dêbki23/IMG_9903.png";
-	std::string path = "C:/Users/marcin/Desktop/test.jpg";
-
-
-	Project1 project(path);
+	std::string path = "C:/Users/marcin/Desktop/lena.png";
+	//Project1_Ar1 project(path);
+	Project1_Mo2 project(path, false);
+	project.dilitation(3);
+	project.print_img();
 	return 0;
 }
